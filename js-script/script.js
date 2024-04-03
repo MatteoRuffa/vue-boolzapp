a0431665601c3bbd5734c20f7fc479ec3e9da2f8
MatteoRuffa <cortese_17@protonmail.com>
2024-04-03 12:42:45 +0200

    add page structure

---
+++ js-script/script.js
@@ -0,0 +1,20 @@
+import {contacts} from './data.js';
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            concatcts: contacts
+        }
+    },
+    methods: {
+            
+    },
+    computed: {
+        
+    },
+    mounted() {
+        
+    }
+}).mount('#app');
